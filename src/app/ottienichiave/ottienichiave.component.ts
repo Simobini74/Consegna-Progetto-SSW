cec124e6bb0f177ee79a146b370db7a82273f4eb
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 08:24:02 +0200

    1 file modified

--- src/app/ottienichiave/ottienichiave.component.ts
+++ src/app/ottienichiave/ottienichiave.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-ottienichiave',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ottienichiave.component.css']
 })
 export class OttienichiaveComponent implements OnInit {
+
+  @Output() newKeyEvent = new EventEmitter<string>();
+  @Output() newAltroEvent = new EventEmitter();
+
+  useKey(key: string) {
+    this.newKeyEvent.emit(key);
+  }
+  getKey(){
+    this.newAltroEvent.emit();
+  }
+
 
   constructor() { }
 
