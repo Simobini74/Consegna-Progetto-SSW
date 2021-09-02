bfc22426948d80acedfba90ab2b0eb5124ecb690
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 07:48:50 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import {OttieniChiaveComponent} from './ottieni-chiave/ottieni-chiave.component';
 import { ChiaveService } from './chiave.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule],
   declarations: [ AppComponent,OttieniChiaveComponent],
   bootstrap:    [ AppComponent ],
   providers: [ChiaveService]
