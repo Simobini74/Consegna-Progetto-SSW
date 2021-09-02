40d3dfcd31ca0fc83090d17d104f65e998b2250e
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 07:44:16 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {OttieniChiaveComponent} from './ottieni-chiave/ottieni-chiave.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent,OttieniChiaveComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
