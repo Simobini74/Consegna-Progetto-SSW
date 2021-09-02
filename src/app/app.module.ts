a90e323f6f0cb26ef72fcf643595cae8ca186fbd
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 07:55:12 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NotificaComponent } from './notifica/notifica.component';
+import { NuovopostComponent } from './nuovopost/nuovopost.component';
 import {OttieniChiaveComponent} from './ottieni-chiave/ottieni-chiave.component';
 import { ChiaveService } from './chiave.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule],
-  declarations: [ AppComponent,OttieniChiaveComponent,NotificaComponent],
+  declarations: [ AppComponent,OttieniChiaveComponent,NotificaComponent,NuovopostComponent],
   bootstrap:    [ AppComponent ],
   providers: [ChiaveService]
 })
---
+++ src/app/nuovopost/nuovopost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuovopost',
+  templateUrl: './nuovopost.component.html',
+  styleUrls: ['./nuovopost.component.css']
+})
+export class NuovopostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
