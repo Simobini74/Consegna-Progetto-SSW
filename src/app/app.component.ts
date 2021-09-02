83c42061e428e143b8da928b0f5fa8f8f20598fc
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 10:39:11 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ export class postit {
   imp: boolean;}
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
--- src/app/ottienichiave/ottienichiave.component.ts
+++ src/app/ottienichiave/ottienichiave.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-ottienichiave',
   templateUrl: './ottienichiave.component.html',
   styleUrls: ['./ottienichiave.component.css']
 })
-export class OttienichiaveComponent implements OnInit {
+export class OttienichiaveComponent  {
 
   @Output() newKeyEvent = new EventEmitter<string>();
   @Output() newAltroEvent = new EventEmitter();
@@ -18,9 +18,5 @@ export class OttienichiaveComponent implements OnInit {
   }
 
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
