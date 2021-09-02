9db198204960fc23cc634824ce12e53577b23831
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 07:26:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jf8pfx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jf8pfx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+
+export class postit {
+  titolo: string;
+  contenuto: string;
+  imp: boolean;}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  title: string = 'Benvenuto! Ti sei loggato con chiave:';
+  selezione: postit = new postit();
+  savedPosts: Array<postit> = [];
+  key = '';
+}
