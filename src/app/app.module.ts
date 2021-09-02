62484218fe8d41ddf48f0249a2003500729f135a
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 08:15:02 +0200

    2 files modified, 5 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NotificaComponent } from './notifica/notifica.component';
 import { NuovopostComponent } from './nuovopost/nuovopost.component';
-import {OttieniChiaveComponent} from './ottieni-chiave/ottieni-chiave.component';
+import {OttienichiaveComponent} from './ottienichiave/ottienichiave.component';
 import { ChiaveService } from './chiave.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule],
-  declarations: [ AppComponent,OttieniChiaveComponent,NotificaComponent,NuovopostComponent],
+  declarations: [ AppComponent,OttienichiaveComponent,NotificaComponent,NuovopostComponent],
   bootstrap:    [ AppComponent ],
   providers: [ChiaveService]
 })
---
+++ src/app/ottienichiave/ottienichiave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ottienichiave',
+  templateUrl: './ottienichiave.component.html',
+  styleUrls: ['./ottienichiave.component.css']
+})
+export class OttienichiaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
