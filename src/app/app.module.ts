2e97bc17eab7b40d987a9ffa2fb17630bfcb4937
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 07:51:18 +0200

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NotificaComponent } from './notifica/notifica.component';
 import {OttieniChiaveComponent} from './ottieni-chiave/ottieni-chiave.component';
 import { ChiaveService } from './chiave.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule],
-  declarations: [ AppComponent,OttieniChiaveComponent],
+  declarations: [ AppComponent,OttieniChiaveComponent,NotificaComponent],
   bootstrap:    [ AppComponent ],
   providers: [ChiaveService]
 })
